152a0cb1a60e93e2b5645a241019ed47d4301ed3
vaxzzzz <victorvaz06@gmail.com.com>
2026-08-31 21:00:17 -0300

    Adição de Spring.Initializr

---
+++ BK/BK/src/test/java/com/induTime/BK/BkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.induTime.BK;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
